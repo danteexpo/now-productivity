0e86ee2fcb74a262ff1990b30d9f7cf49655b385
Dante Expo <dante0im@gmail.com>
2022-06-12 16:12:56 -0300

    created app

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,47 @@
-import '../styles/globals.css'
+import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default MyApp;
+
+/*
+// ngss
+export const getServerSideProps = async context => {
+  return {
+    props: {},
+  };
+};
+
+// ngsp
+export const getStaticProps = async context => {
+  return {
+    props: {},
+  };
+};
+
+// ngspa
+export const getStaticPaths = async () => {
+  return {
+    paths:[],
+    fallback:false
+  }
+}
+
+// ncdoc
+import { Html, Main, NextScript } from "next/document";
+
+const Document = () => {
+  return (
+    <Html lang="en">
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+};
+
+export default Document;
+*/
